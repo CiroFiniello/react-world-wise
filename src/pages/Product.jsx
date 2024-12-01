15e5f92e0f6c7f7e2e9521b2e60e620b1adfe4eb
CiroFiniello <kirofiniello@outlook.it>
2024-12-01 17:03:09 +0100

    ho aggiunto la cartella componenti e il componente pageNav , che gestisce la navigazione attraverso NavLink, per ottenere la SPA, poi ho importato in ogni componente la relativa nav e testato la funzionalità

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,12 @@
+import PageNav from "../components/PageNav";
+
 function Product() {
-  return <div>product</div>;
+  return (
+    <div>
+      <PageNav />
+      product
+    </div>
+  );
 }
 
 export default Product;
