ac3a01e917a51aec05efa62656f28ddb6857364b
CiroFiniello <kirofiniello@outlook.it>
2024-12-05 18:11:49 +0100

    ho creato la funzione run per il server "faker" all'interno del package.json, ho gestito i dati all'interno dell'app, e creato un componente cityItem,  al quale ho passato la citylist, per renderizzare le cities, poi ho creato un componente message per un messaggio di errore nel momento in cui verranno cancellati gli item dalla lista

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,31 @@ import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
@@ -16,8 +39,15 @@ function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="/login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>LIST</p>} />
-            <Route index path="cities" element={<p>Cities</p>} />
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              index
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
             <Route path="countries" element={<p>Countries</p>} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
